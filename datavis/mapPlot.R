c4f7d42d53cedfa979b8ad67dfc16337d27aaeb4
martind <martin@dekstop.de>
2012-07-30 09:43:06 +0100

    tabs to spaces

--- datavis/mapPlot.R
+++ datavis/mapPlot.R
@@ -20,28 +20,28 @@ library(gridExtra)
 gpclibPermit()
 
 stratify <- function(data, grid, offset=0) {
-	transform(data, 
-		Lon = (Lon - offset) - ((Lon - offset) %% grid) + offset, 
-		Lat = (Lat - offset) - ((Lat - offset) %% grid) + offset)
+  transform(data, 
+    Lon = (Lon - offset) - ((Lon - offset) %% grid) + offset, 
+    Lat = (Lat - offset) - ((Lat - offset) %% grid) + offset)
 }
 
 stratified_hist <- function(data_stratified) {
-	h = aggregate(
-		data.frame(Lat=data_stratified$Lat, Lon=data_stratified$Lon), 
-		list(SLat=data_stratified$Lat, SLon=data_stratified$Lon), 
-		length)
-	data.frame(Lat=h$SLat, Lon=h$SLon, Count=h$Lat)
+  h = aggregate(
+    data.frame(Lat=data_stratified$Lat, Lon=data_stratified$Lon), 
+    list(SLat=data_stratified$Lat, SLon=data_stratified$Lon), 
+    length)
+  data.frame(Lat=h$SLat, Lon=h$SLon, Count=h$Lat)
 }
 
 basemap <- function(world) {
   ggplot(world, aes(long, lat, group=group)) + 
-		geom_polygon(size=0.2, fill=I('#ffffff'), col=I('#bbbbbb')) +
-  	coord_equal() + 
-  	# opts(aspect.ratio = 1) + 
-  	opts(panel.background = theme_blank(), 
+    geom_polygon(size=0.2, fill=I('#ffffff'), col=I('#bbbbbb')) +
+    coord_equal() + 
+    # opts(aspect.ratio = 1) + 
+    opts(panel.background = theme_blank(), 
       # legend.position = "none",
-  		panel.grid.minor=theme_blank(), panel.grid.major=theme_blank(), 
-  		axis.ticks = theme_blank(), axis.text.x = theme_blank(), axis.title.x=theme_blank(), axis.text.y = theme_blank(), axis.title.y=theme_blank())
+      panel.grid.minor=theme_blank(), panel.grid.major=theme_blank(), 
+      axis.ticks = theme_blank(), axis.text.x = theme_blank(), axis.title.x=theme_blank(), axis.text.y = theme_blank(), axis.title.y=theme_blank())
 }
 
 prepareCosmData <- function(d, tagStr) {
@@ -57,9 +57,9 @@ mapPlot <- function(mapdata, basemap, title) {
   
   basemap + 
     geom_point(data=mapdata, aes(group=1, x=mapdata$Lon, y=mapdata$Lat, cex=mapdata$Count), col='#ff0000', alpha=0.5, type='p', pch=19) +
-		scale_size(range=c(0.3,4), name="Sensors") +
+    scale_size(range=c(0.3,4), name="Sensors") +
     coord_map(project="mercator", xlim=c(-170,170), ylim=c(-55,70)) +
-		opts(title=title)
+    opts(title=title)
     # coord_cartesian(xlim=c(min(mapdata$Lon), max(mapdata$Lon)), ylim=c(min(mapdata$Lat), max(mapdata$Lat)))
 }
   
