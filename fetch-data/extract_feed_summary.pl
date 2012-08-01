d52dcd9523c6505fe28105581e2d86453f2bb654
martind <martin@dekstop.de>
2012-08-01 12:52:21 +0100

    adding more fields to feed index file

--- fetch-data/extract_feed_summary.pl
+++ fetch-data/extract_feed_summary.pl
@@ -43,6 +43,7 @@ if ($nodes->size) {
 	print STDERR "Found ", $nodes->size, " environments.\n";
 	foreach my $node ($nodes->get_nodelist) {
 		my $id = get_data_item($xpath, '@id', $node);
+		my $creator = get_data_item($xpath, '@creator', $node);
 		my $created_at = get_data_item($xpath, '@created', $node);
 		my $updated_at = get_data_item($xpath, '@updated', $node);
 		my $title = get_data_item($xpath, 'title/text()', $node);
@@ -50,6 +51,9 @@ if ($nodes->size) {
 		my $status = get_data_item($xpath, 'status/text()', $node);
 		my $private = get_data_item($xpath, 'private/text()', $node);
 		my $location_name = get_data_item($xpath, 'location/name/text()', $node);
+		my $location_domain = get_data_item($xpath, 'location/@domain', $node);
+		my $location_exposure = get_data_item($xpath, 'location/@exposure', $node);
+		my $location_disposition = get_data_item($xpath, 'location/@disposition', $node);
 		my $location_lat = get_data_item($xpath, 'location/lat/text()', $node);
 		my $location_lon = get_data_item($xpath, 'location/lon/text()', $node);
 		
@@ -64,11 +68,11 @@ if ($nodes->size) {
 			my $stream_last_sample_time = get_data_item($xpath, 'current_value/@at', $_);
 			my @stream_tags = get_data_list($xpath, 'tag/text()', $_);
 			
-			# ID	CREATED_AT	TITLE	FEED	STATUS	PRIVATE	LOCATION	LAT	LON	ENV_TAGS	STREAM_ID	STREAM_UNIT	STREAM_TIMESTAMP	STREAM_TAGS
+			# ID	CREATOR	CREATED_AT	UPDATED_AT	TITLE	FEED	STATUS	PRIVATE	LOCATION	LOCATION_DOMAIN	LOCATION_EXPOSURE	LOCATION_DISPOSITION	LAT	LON	ENV_TAGS	STREAM_ID	STREAM_UNIT	STREAM_TIMESTAMP	STREAM_TAGS	ALL_TAGS
 
 			# First: env metadata, repeated once per stream
 			my $jtags = join(", ", sort(@env_tags));
-			print "${id}\t${created_at}\t${updated_at}\t${title}\t${feed}\t${status}\t${private}\t${location_name}\t${location_lat}\t${location_lon}\t${jtags}\t";
+			print "${id}\t${creator}\t${created_at}\t${updated_at}\t${title}\t${feed}\t${status}\t${private}\t${location_name}\t${location_domain}\t${location_exposure}\t${location_disposition}\t${location_lat}\t${location_lon}\t${jtags}\t";
 			
 			# Followed up by stream data and metadata
 			$jtags = join(", ", sort(@stream_tags));
