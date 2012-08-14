e71c90e263ab923853c7628535d5d067b92e39f6
martind <martin@dekstop.de>
2012-08-14 11:48:37 +0200

    table name change

--- rb-scraper/schedule.rb
+++ rb-scraper/schedule.rb
@@ -17,7 +17,7 @@ conn = PGconn.open(
 	:user => @env[:user], 
 	:password => @env[:password])
 
-res = conn.exec('SELECT id FROM environments ORDER BY id')
+res = conn.exec('SELECT id FROM environment ORDER BY id')
 environments = {}
 idx = 0
 res.each do |row|
@@ -29,7 +29,7 @@ end
 environments.values.each do |batch|
 	res = conn.exec('INSERT INTO requests(envid, scheduleid) 
 		SELECT e.id, s.id 
-		FROM environments e 
+		FROM environment e 
 		CROSS JOIN schedule s 
 		LEFT OUTER JOIN requests r ON (r.envid=e.id and r.scheduleid=s.id)
 		WHERE s.request
