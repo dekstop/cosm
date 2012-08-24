096f1f9034a09f895a2a49488796b9b849850651
martind <martin@dekstop.de>
2012-08-24 22:48:48 +0100

    initial commit

---
+++ py-analysis/plot-variance.py
@@ -0,0 +1,305 @@
+# Overlaid sparkline plots to visually show variances in related time series data streams.
+#
+# Notes:
+# - we're computing the coefficient of variation for sensor values. This is not appropriate in
+#   a number of cases:
+#   - when values are approaching zero, in which case the CoV will approach infinity
+#   - when values are on an interval scale, not ratio scale (when values can be negative), in
+#     which case the CoV may not be meaningful
+# TODO:
+# - horizontal alignment of numbers at their decimal points, regardless of formats and suffixes
+# - formatting: add number scales for fractions too -- atm I don't think my data warrants it.
+#
+# Martin Dittus, 2012
+# 
+
+import argparse
+from collections import defaultdict
+import csv
+import math
+import os.path
+import sys
+
+import numpy as np
+
+import matplotlib
+matplotlib.use('PDF')
+# matplotlib.use('macosx')
+
+import matplotlib.pyplot as plt
+from pylab import randn, sin
+
+# =========
+# = Tools =
+# =========
+
+# For filtering
+def isValue(v):
+    return (v is not None) and (np.isnan(v)==False)
+
+# remove all "None"s
+def onlyValues(values):
+    return filter(isValue, values)
+
+# p is [0..1]
+def percentile(data, p, limit=()):
+    values = sorted(onlyValues(data))
+    if (len(values)==0):
+        return float('nan')
+    if (len(values)==1):
+        return data[0]
+    if limit:
+        values = [min(max(value, limit[0]), limit[1]) for value in values]
+
+    idx = p * (len(values) - 1)
+    if (idx % 1 == 0):
+        return values[0]
+    else:
+        a = values[int(idx)]
+        b = values[int(idx) + 1]
+        return a + (b - a) * (idx % 1)
+
+# Coefficient of variation
+def cov(sd, mean):
+    if (mean==0):
+        return 0
+    return sd / mean
+
+# =================
+# = Number Format =
+# =================
+
+# Scales this value to below 10,000
+# Returns the value and an SI symbol suffix
+def scaleValue(v):
+    suffixes = ['', 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y']
+    sidx = 0
+    while sidx<len(suffixes) and v >= (1000 * 10):
+        v = v / 1000
+        sidx = sidx + 1
+    return (v, suffixes[sidx])
+
+# Formats to four significant digits.
+# Assumes 10,000 > v >= 0.001
+def formatDigits(v):
+    if v==0:
+        return "0.0"
+    if v>=1000:
+        format = "{:,.0f}"
+    elif v>=100:
+        format = "{:.1f}"
+    elif v>=10:
+        format = "{:.2f}"
+    else:
+        format = "{:.3f}"
+    return format.format(v)
+
+# Not needed -- Python 2.7 formatting added a thousands separator!
+# # Takes a numeric string
+# # Inserts commas at all the appropriate places
+# def formatThousands(fv):
+#     text = ''
+#     if (fv.find('.')>0):
+#         digits = fv[:fv.find('.')]
+#         frac = fv[fv.find('.'):]
+#     else:
+#         digits = fv
+#         frac = ''
+#     ndigits = len(digits)
+#     nsegments = ndigits / 3
+#     rem = ndigits % 3
+#     if (rem>0):
+#         text = digits[:rem] + ','
+#         ofs = 1
+#     else:
+#         ofs = 0
+#     for i in range(nsegments-ofs):
+#         text += digits[rem:rem+3] + ','
+#         rem += 3
+#     text += digits[rem:rem+3]
+#     return text + frac
+
+# It's always hard, this.
+# To test:
+# values = [
+#     (0.0, "0.0"),
+#     (0.0001234, "0.000"), 
+#     (0.0012345, "0.001"),
+#     (0.0123456, "0.012"),
+#     (0.1234567, "0.123"),
+#     (1.2345, "1.235"),
+#     (12.345, "12.35"),
+#     (123.45, "123.5"),
+#     (1234.5, "1,235"),
+#     (12345.67890, "12.35K"),
+#     (123456.7890, "123.5K"),
+#     (1234567.890, "1,235K"),
+#     (12345678.90, "12.35M"),
+#     (123456789.0, "123.5M"),
+#     (1234567890.1234, "1,235M"),
+#     (12345678901.234, "12.35B"),
+#     (123456789012.34, "123.5B"),
+#     (1234567890123.4, "1,235B"),
+#     (12345678901234.5, "12.35T"),
+# ]
+# for a, b in values:
+#     print a, formatLabel(a), b
+#     # assert formatLabel(a) == b, ("%s != %s" % (formatLabel(a), b))
+def formatLabel(v):
+    if isValue(v)==False:
+        return '(no numbers)'
+    v, suffix = scaleValue(v)
+    return formatDigits(v) + suffix
+
+# ========
+# = Main =
+# ========
+
+if __name__ == "__main__":
+    from decimal import Decimal
+    
+    defaultWidth = 2
+    defaultHeight = 0.4
+    defaultDpi = 80
+    defaultFontsize = 6
+        
+    parser = argparse.ArgumentParser(description='Create a timeseries plot for multiple data streams.')
+    parser.add_argument('filename', help='TSV file of (group, date, item, val1, val2, ...)')
+    parser.add_argument('validx', help='index of column to plot')
+    parser.add_argument('outfilename', help='PDF filename')
+    parser.add_argument('-w', '--width', dest='width', action='store', type=float, 
+        default=defaultWidth, help='width (in inches)')
+    parser.add_argument('-e', '--height', dest='height', action='store', type=float, 
+        default=defaultHeight, help='height (in inches)')
+    parser.add_argument('-d', '--dpi', dest='dpi', action='store', type=float, 
+        default=defaultWidth, help='dpi')
+    parser.add_argument('-f', '--font-size', dest='fontsize', action='store', type=float, 
+        default=defaultFontsize, help='font size in points')
+    args = parser.parse_args()
+    
+    if (os.path.isfile(args.filename)==False):
+        print "File doesn't exist: %s" % (args.filename)
+        sys.exit(1)
+
+    # Load
+    data = dict()
+    allDates = set()
+    reader = csv.reader(open(args.filename, 'rb'), delimiter='	', quoting=csv.QUOTE_NONE)
+    for rec in reader:
+        item = rec[2]
+        date = rec[1]
+        str = rec[int(args.validx)]
+        if (str==''):
+            val = None
+        else:
+            try:
+                val = float(str)
+            except ValueError:
+                print rec
+                raise
+                # sys.exit(1)
+        if (item in data)==False:
+            data[item] = defaultdict(lambda: None)
+        data[item][date] = val
+        allDates.add(date)
+
+    print "%d data streams and %d dates" % (len(data.keys()), len(allDates))
+
+    # Prepare data
+    dates = sorted(allDates)
+    timeseries = []
+    for item in data.keys():
+        x = [data[item][date] for date in dates]
+        timeseries.append(x)
+
+    # summary stats: sd, mean, coefficient of variation
+    sdeviations = [np.std(onlyValues(values)) for values in zip(*timeseries)]
+    means = [np.mean(onlyValues(values)) for values in zip(*timeseries)]
+    covs = [cov(sd, mean) for sd, mean in zip(sdeviations, means)]
+
+    numpoints = len(means)
+    meanval = np.mean(means)
+    # medianval = np.median(means)
+
+    # variances = [np.var(onlyValues(values)) for values in zip(*timeseries)]
+    meansd = np.mean(sdeviations)
+    meancov = np.mean(covs)
+    mincov = min(covs)
+    mincovpos = covs.index(mincov)
+    maxcov = max(covs)
+    maxcovpos = covs.index(maxcov)
+    
+    # Graph
+    figsize = (args.width, args.height)
+    fig = plt.figure(figsize=figsize, dpi=args.dpi)
+    ax1 = plt.axes(frameon=False)
+
+    ax1.axes.get_xaxis().set_visible(False)
+    ax1.axes.get_yaxis().set_visible(False)
+
+    # graph dimensions
+    # values = 
+    noData = (len(onlyValues([x for sublist in timeseries for x in sublist]))==0)
+    if noData:
+        lower = 0
+        higher = 0
+    else:
+        lower = min(onlyValues([percentile(onlyValues(values), .01) for values in zip(*timeseries)]))
+        higher = max(onlyValues([percentile(onlyValues(values), .99) for values in zip(*timeseries)]))
+    if math.isnan(lower):
+        lower = 0
+    if math.isnan(higher):
+        higher = 0
+    if lower==higher:
+        if lower==0.0:
+            lower = -0.00000001
+            higher = 0.00000001
+        higher = higher * 1.1
+        lower = lower * 0.9
+    width = numpoints * 1.3
+    # height = maxval - minval
+    height = higher - lower
+    ymargin = height * 0.2
+    textpos = numpoints * 1.3
+    textVOffset = - height * 0.15
+
+    ax1.axes.set_xlim(0, width)
+    ax1.axes.set_ylim(
+        lower - ymargin, 
+        higher + ymargin)
+
+    # Plots
+    for ts in timeseries:
+        allNone = len(onlyValues(ts))==0
+        if (allNone or len(ts)==0):
+            # plt.plot([0] * len(allDates), marker='x', color='#999999', linewidth=0.5)
+            pass
+        else:
+            plt.plot(ts, color='#999999', alpha=0.3, linewidth=0.2)
+    if noData==False:
+        plt.plot(means, color='#333333', linewidth=0.5)
+
+    # Labels
+    plt.text(width, higher + textVOffset, len(timeseries), 
+        size=args.fontsize, color='#999999', 
+        horizontalalignment='right', verticalalignment='bottom')
+    if noData==False:
+        plt.text(width, (lower+higher)/2 + textVOffset, formatLabel(meanval), 
+            size=args.fontsize, color='#999999', 
+            horizontalalignment='right', verticalalignment='bottom')
+    plt.text(width, lower + textVOffset, formatLabel(meancov), 
+        size=args.fontsize, color='#333333', 
+        horizontalalignment='right', verticalalignment='bottom')
+
+    # Dots
+    if isValue(maxcov):
+        plt.plot([maxcovpos], [higher + ymargin], '.', 
+            color='#ff6666', alpha=1, 
+            markeredgewidth=10000, clip_on=False)
+    if isValue(mincov):
+        plt.plot([mincovpos], [lower - ymargin], '.', 
+            color='#6666ff', alpha=1, 
+            markeredgewidth=100000, clip_on=False)
+
+    # Done.
+    plt.savefig(args.outfilename, bbox_inches='tight')
---
+++ py-analysis/score-synonyms.py
@@ -0,0 +1,260 @@
+# Tag synonymity scores based on tag, item, user relationships
+# Clements et al (2008), "Detecting Synonyms in Social Tagging Systems to Improve Content Retrieval"
+#
+# Martin Dittus, 2012
+# 
+# Notes:
+# - we're using sparse matrices and heavily depend on their internals for performance optimisations.
+# - in this script a "tag" is an ID that can be used as index position into data structures.
+# - note that matrices rely on positive integers [0..n] for cell access. if needed there's an optional 
+#   ability to convert your tag/user/item identifiers to such numeric IDs. ("--as-strings")
+# - this script doesn't include a "min number of tagged items per user" threshold, we assume that has already
+#   been applied to the input data.
+#
+# TODO
+# - split into two scripts: compute tag similarity (run once per matrix), and synonym scores
+# - benchmark: fast row extraction/conversion to list
+# 
+
+import argparse
+import csv
+import os.path
+import sys
+
+import numpy as np
+
+from scipy.sparse import lil_matrix
+from scipy.stats import *
+
+
+# ======================
+# = Fast Sparse Matrix =
+# ======================
+
+# A sparse matrix optimised for fast row access operations.
+class HMatrix(lil_matrix):
+
+    # Returns a dense array of all values in a row.
+    def getRow(self, rowIdx):
+        return self[rowIdx,:].toarray()[0]
+        # t = np.array([0] * self.shape[1]) # length of num_cols, all 0 values
+        # idx = self.rows[rowIdx]
+        # data = self.data[rowIdx]
+        # t[idx] = data
+        # # t = [0] * self.shape[1]
+        # # for i,v in zip(idx, data):
+        # #     t[i] = v
+        # return t
+
+    # Count the number of cells in a row that have a value.
+    def numValuesInRow(self, rowIdx):
+        return len(self.rows[rowIdx])
+
+    # Remove the values of several rows from a sparse matrix.
+    # Needed since sparse matrices have no fancy indexing; otherwise we could e.g. do m[(1,2,3),:] = 0
+    # Additionally this avoids wasting cycles on comparisons later.
+    def removeRows(self, rows):
+        for r in rows:
+            self.rows[r] = []
+            self.data[r] = []
+    
+    def reshape(self, shape):
+        return HMatrix(super(HMatrix, self).reshape(shape))
+        
+# =========
+# = Tools =
+# =========
+
+# Construct and remember numeric IDs for string lists
+class IdMapping:
+    
+    def __init__(self):
+        self.counter = 0
+        self.namesToId = dict()
+        self.idsToNames = dict()
+    
+    def getOrMakeId(self, str):
+        if (str in self.namesToId.keys()):
+            return self.namesToId[str]
+        self.counter += 1
+        id = self.counter
+        self.namesToId[str] = id
+        self.idsToNames[id] = str
+        return id
+    
+    def getName(self, id):
+        return self.idsToNames[id]
+    
+    def __str__(self):
+        return str(self.namesToId)
+
+# Reads TSV files of (item, tag, count) or (user, tag, count)
+# Returns a 2D sparse matrix of structure (tag, item) or (tag, user)
+# Expects that tag, item, and user identifiers are positive ints
+# If they're not: can optionally maintain translation tables and accept strings too.
+def readTagItemMatrix(filename, asStrings, amap=None, tagmap=None):
+    print "Loading", filename
+    reader = csv.reader(open(filename, 'rb'), delimiter='	', quoting=csv.QUOTE_NONE)
+    m = HMatrix((2,2)) # optimised for row-based access
+    
+    for a, tag, count in reader:
+        if (asStrings==True):
+            a = amap.getOrMakeId(a)
+            tag = tagmap.getOrMakeId(tag)
+        else:
+            a = int(a)
+            tag = int(tag)
+        count = float(count)
+
+        # ensure matrix is big enough to contain all IDs
+        newShape = m.shape
+        while (tag>=newShape[0]):
+            newShape = (newShape[0]*2, newShape[1])
+        while (a>=newShape[1]):
+            newShape = (newShape[0], newShape[1]*2)
+        if (newShape != m.shape):
+            print "Resizing matrix to", newShape
+            m = m.reshape(newShape)
+
+        m[tag,a] = count
+    
+    return m
+
+# ===========
+# = Scoring =
+# ===========
+
+# Takes a 2D sparse matrix of shape (tag, item) or (tag, user)
+# Returns a set of tags that have been used less than minUses times (by users, or on items)
+def getRarelyUsedTags(m, minUses):
+    ft = set()
+    tags, items = m.nonzero() # only process tags that have been used
+    tags = set(tags) # uniques
+    for tag in tags:
+        if (m.numValuesInRow(tag) < minUses):
+            ft.add(tag)
+    return ft
+
+# Takes a 2D sparse matrix of shape (tag, item) or (tag, user)
+# Computes pearson correlation coefficients for tag pairs
+# Returns a sparse matrix of (tag, tag)
+def tagSimilarityScores(m):
+    sim = HMatrix((m.shape[0], m.shape[0])) # tag-tag similarity matrix
+    utags, uitems = m.nonzero() # only process tags that have been used
+    utags = set(utags) # uniques
+    for tag1 in utags:
+    # for tag1 in xrange(m.shape[0]):
+        tag1Items = m.getRow(tag1)
+        # for tag2 in utags:
+        tags2 = utags.intersection(xrange(tag1+1, m.shape[0]))
+        print "%s: %d comparisons" % (tag1, len(tags2))
+        for tag2 in tags2:
+            tag2Items = m.getRow(tag2)
+            c, p = pearsonr(tag1Items, tag2Items)
+            sim[tag1, tag2] = c
+    return sim
+
+# ========
+# = Main =
+# ========
+
+# mapping from assigned IDs back to the original strings
+umap = IdMapping()
+imap = IdMapping()
+tmap = IdMapping()
+
+if __name__ == "__main__":
+    
+    defaultMinUsersPerTag = 1
+    defaultMinItemsPerTag = 1
+    defaultMinP = 0.0 # minimum p-value for pearson's correlation coefficient
+    defaultMinISim = 0.5
+    defaultMaxUSim = 0.0
+    
+    parser = argparse.ArgumentParser(description='Compute a synonymity score for tags.')
+    parser.add_argument('utmfile', help='TSV file of (userid, tagid, number of items)')
+    parser.add_argument('itmfile', help='TSV file of (itemid, tagid, number of users)')
+    parser.add_argument('outfile', help='TSV file to store (tagid, tagid, score)')
+    parser.add_argument('--as-strings', dest='asStrings', action='store_true', 
+        help='treat IDs as strings, not positive integers')
+    parser.add_argument('-u', '--min-users-per-tag', dest='minUsersPerTag', type=int,
+        action='store', default=defaultMinUsersPerTag,
+        help='minimum number of users per tag')
+    parser.add_argument('-i', '--min-items-per-tag', dest='minItemsPerTag', type=int,
+        action='store', default=defaultMinItemsPerTag,
+        help='minimum number of items per  tag')
+    parser.add_argument('-p', '--min-p', dest='minP', type=float,
+        action='store', default=defaultMinP,
+        help='minimum 2-tailed p-value for the Pearson correlation coefficient')
+    parser.add_argument('-x', '--max-user-similarity', dest='maxUSim', type=float,
+        action='store', default=defaultMaxUSim,
+        help='maximum user similarity score')
+    parser.add_argument('-y', '--min-item-similarity', dest='minISim', type=float,
+        action='store', default=defaultMinISim,
+        help='minimum item similarity score')
+    args = parser.parse_args()
+    
+    if (os.path.isfile(args.utmfile)==False):
+        print "File doesn't exist: %s" % (args.utmfile)
+        sys.exit(1)
+
+    if (os.path.isfile(args.itmfile)==False):
+        print "File doesn't exist: %s" % (args.itmfile)
+        sys.exit(1)
+
+    # Load
+    if (args.asStrings):
+        utm = readTagItemMatrix(args.utmfile, args.asStrings, umap, tmap)
+        itm = readTagItemMatrix(args.itmfile, args.asStrings, imap, tmap)
+
+        # print "Mappings: "
+        # print umap
+        # print imap
+        # print tmap
+        # print "------"
+    else:
+        utm = readTagItemMatrix(args.utmfile, args.asStrings)
+        itm = readTagItemMatrix(args.itmfile, args.asStrings)
+    
+    # Apply thresholds
+    print "Determining tags below threshold"
+    lowUTags = getRarelyUsedTags(utm, args.minUsersPerTag)
+    lowITags = getRarelyUsedTags(itm, args.minItemsPerTag)
+    lowTags = lowUTags.union(lowITags)
+    
+    print "Removing %s tags below threshold" % (len(lowUTags))
+    utm.removeRows(lowTags)
+    itm.removeRows(lowTags)
+    
+    # Compute user and item similarity between tag pairs
+    print "Computing user similarity"
+    usimilarity = tagSimilarityScores(utm)
+
+    print "Computing item similarity"
+    isimilarity = tagSimilarityScores(itm)
+    
+    # Result
+    print "Writing", args.outfile
+    writer = csv.writer(open(args.outfile, 'wb'), delimiter='\t', quoting=csv.QUOTE_NONE)
+    allwriter = csv.writer(open(args.outfile + ".all", 'wb'), delimiter='\t', quoting=csv.QUOTE_NONE)
+
+    tags1, tags2 = usimilarity.nonzero()  # only existing (tag, tag) pairs
+    for tag1, tag2 in zip(tags1, tags2):
+
+        usim = usimilarity[tag1, tag2]
+        isim = isimilarity[tag1, tag2]
+
+        if (args.asStrings):
+            rec = (
+                tmap.getName(tag1), 
+                tmap.getName(tag2), 
+                usim, 
+                isim)
+        else:
+            rec = (tag1, tag2, usim, isim)
+        
+        allwriter.writerow(rec)
+        
+        if usim < args.maxUSim and isim >= args.minISim:
+            print rec
+            writer.writerow(rec)
